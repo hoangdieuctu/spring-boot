14405711bb95d7664f3890628dc7279adce55fe7
Nguyen Hoang Dieu <dnguyen@karrostech.com>
2020-03-23 22:18:23 +0700

    init project spring boot admin

---
+++ actuator-and-admin/admin-client/src/main/java/com/hoangdieuctu/boot/actuator/ServerApplication.java
@@ -0,0 +1,29 @@
+package com.hoangdieuctu.boot.actuator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ServerApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(ServerApplication.class);
+
+    @GetMapping("/log")
+    public void log() {
+        logger.trace("TRACE message");
+        logger.debug("DEBUG message");
+        logger.info("INFO message");
+        logger.warn("WARNING message");
+        logger.error("ERROR message");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
